2b69b850623ee909e0a868831f1bb5f33b63c730
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-02-21 15:18:17 +0100

    Fix form search enter trigger

--- resources/views/web/home/index.blade.php
+++ resources/views/web/home/index.blade.php
@@ -7,11 +7,12 @@
         <h1 class="display-4 text-white text-center">Fluid jumbotron</h1>
         <p class="lead text-white text-center">This is a modified jumbotron that occupies the
             entire horizontal space of its parent.</p>
-        <form class="justify-content-center">
+        <form id="searchForm" class="justify-content-center">
             <div class="input-group justify-content-center">
                 <input type="text" class="border border-right-0 form-control-lg w-75" placeholder="Search...">
                 <span class="input-group-append">
-                    <button class="btn btn-outline-secondary bg-primary border-left-0" type="button">
+                    <button class="btn btn-outline-secondary bg-primary border-left-0" 
+                    type="button" disabled>
                         <i class="fa fa-search text-white"></i>
                     </button>
                 </span>
