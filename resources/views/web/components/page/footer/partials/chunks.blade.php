e3189614f44633fe56e11167209da6723ad4a3eb
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-07-02 19:53:18 +0200

    refactor

--- resources/views/web/components/page/footer/partials/chunks.blade.php
+++ resources/views/web/components/page/footer/partials/chunks.blade.php
@@ -4,7 +4,10 @@
     <div class="col-md-3 col-sm-6">
         <div class="list-group list-group-flush mb-3">
             @include('icore::web.components.page.footer.partials.pages', [
-                'pages' => $pages->whereIn('id', $chunk)->sortBy(function($item) use ($chunk) { return array_search($item->id, $chunk); })
+                'pages' => $pages->whereIn('id', $chunk)
+                    ->sortBy(function($item) use ($chunk) { 
+                        return array_search($item->id, $chunk); 
+                    })
             ])
         </div>
     </div>
