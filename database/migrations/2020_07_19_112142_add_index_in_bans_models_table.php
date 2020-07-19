5b6c4a5b6edf80ee30ab7a02609ccf24252b3ffe
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-07-19 15:06:02 +0200

    add index

---
+++ database/migrations/2020_07_19_112142_add_index_in_bans_models_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RepairIndexInBansModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bans_models', function (Blueprint $table) {
+            $table->index(['model_id', 'model_type'], 'bans_models_model_id_model_type_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bans_models', function (Blueprint $table) {
+            $table->dropIndex('bans_models_model_id_model_type_index');
+        });
+    }
+}
