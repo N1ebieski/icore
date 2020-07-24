89f41a12264ead8c36e09c80d4a24653745bfcb8
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-07-24 11:38:54 +0200

    fix h5

--- resources/views/web/components/link.blade.php
+++ resources/views/web/components/link.blade.php
@@ -1,5 +1,5 @@
-<h5 class="mt-3 mb-2">{{ trans('icore::links.route.index') }}:</h5>
 @if ($links->isNotEmpty())
+<h5 class="mt-3 mb-2">{{ trans('icore::links.route.index') }}:</h5>
 <ul class="list-group list-group-flush">
     @foreach ($links as $link)
     <li class="list-group-item">
