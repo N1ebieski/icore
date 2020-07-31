8aacebd9f550cc021315849745b596bf42b3147e
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-07-31 22:44:43 +0200

    fix

--- resources/views/web/components/category/post/partials/categories.blade.php
+++ resources/views/web/components/category/post/partials/categories.blade.php
@@ -4,7 +4,9 @@
         title="{{ $category->name }}"
         class="{{ $isUrl(route('web.category.post.show', $category->slug), 'font-weight-bold') }}">
             <span>{{ str_repeat('-', $category->real_depth) }}</span>
-            @if (!empty($category->icon))<i class="{{ $category->icon }}"></i>;@endif
+            @if (!empty($category->icon))
+            <i class="{{ $category->icon }} text-center" style="width:1.5rem"></i>;
+            @endif
             <span>{{ $category->name }}</span>
         </a>
         <span class="badge badge-primary badge-pill">{{ $category->morphs_count }}</span>
