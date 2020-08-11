2503e039407e94f9665411ff2044a58a7b9da04d
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-08-11 13:26:38 +0200

    add polymorphic morphs relation

--- src/Models/Tag/Post/Tag.php
+++ src/Models/Tag/Post/Tag.php
@@ -2,10 +2,24 @@
 
 namespace N1ebieski\ICore\Models\Tag\Post;
 
+use Illuminate\Support\Facades\Config;
 use N1ebieski\ICore\Models\Tag\Tag as BaseTagModel;
 
 class Tag extends BaseTagModel
 {
+    // Relations
+
+    /**
+     * [morphs description]
+     * @return [type] [description]
+     */
+    public function morphs()
+    {
+        $table = Config::get('taggable.tables.taggable_taggables', 'taggable_taggables');
+
+        return $this->morphedByMany(\N1ebieski\ICore\Models\Post::class, 'taggable', $table, 'tag_id');
+    }
+
     // Accessors
 
     /**
