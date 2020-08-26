5eb91f3021dae22ff369f81c4881d0008dfb623e
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-08-27 00:08:09 +0200

    fix

--- src/Providers/ScheduleServiceProvider.php
+++ src/Providers/ScheduleServiceProvider.php
@@ -38,11 +38,11 @@ class ScheduleServiceProvider extends ServiceProvider
         }
 
         if ($hours < 24) {
-            $cron = "0 */{$hours} * * *";
+            $cron = "1 */{$hours} * * *";
         } else {
             $days = ceil($hours/24);
 
-            $cron = "0 0 */{$days} * *";
+            $cron = "1 0 */{$days} * *";
         }
 
         $this->schedule->command('cache:clear')
