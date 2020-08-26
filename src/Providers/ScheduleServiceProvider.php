5e2726e145e67243c756e13d911cd41677cd8a6e
N1ebieski <mariusz.wysokinski@neostrada.pl>
2020-08-26 19:03:01 +0200

    add cache clear scheduler

--- src/Providers/ScheduleServiceProvider.php
+++ src/Providers/ScheduleServiceProvider.php
@@ -5,11 +5,15 @@ namespace N1ebieski\ICore\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Console\Scheduling\Schedule;
 
-/**
- * [ScheduleServiceProvider description]
- */
 class ScheduleServiceProvider extends ServiceProvider
 {
+    /**
+     * Undocumented variable
+     *
+     * @var Schedule
+     */
+    protected $schedule;
+
     /**
      * Register services.
      *
@@ -21,6 +25,33 @@ class ScheduleServiceProvider extends ServiceProvider
     }
 
     /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    protected function prepareClearCacheSchedule() : void
+    {
+        $hours = ceil($this->app['config']->get('cache.minutes')/60);
+
+        if ($hours <= 0 || $hours > 672) {
+            return;
+        }
+
+        if ($hours < 24) {
+            $cron = "0 */{$hours} * * *";
+        } else {
+            $days = ceil($hours/24);
+
+            $cron = "0 0 */{$days} * *";
+        }
+
+        $this->schedule->command('cache:clear')
+            ->name('ClearCache')
+            ->cron($cron)
+            ->runInBackground();
+    }
+
+    /**
      * Bootstrap services.
      *
      * @return void
@@ -28,24 +59,26 @@ class ScheduleServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->booted(function () {
-            $schedule = $this->app->make(Schedule::class);
+            $this->schedule = $this->app->make(Schedule::class);
 
-            $schedule->call($this->app->make(\N1ebieski\ICore\Crons\MailingCron::class))
+            $this->prepareClearCacheSchedule();
+
+            $this->schedule->call($this->app->make(\N1ebieski\ICore\Crons\MailingCron::class))
                 ->name('MailingCron')
                 ->everyThirtyMinutes()
                 ->runInBackground();
 
-            $schedule->call($this->app->make(\N1ebieski\ICore\Crons\PostCron::class))
+            $this->schedule->call($this->app->make(\N1ebieski\ICore\Crons\PostCron::class))
                 ->name('PostCron')
                 ->everyThirtyMinutes()
                 ->runInBackground();
 
-            $schedule->call($this->app->make(\N1ebieski\ICore\Crons\Sitemap\SitemapCron::class))
+            $this->schedule->call($this->app->make(\N1ebieski\ICore\Crons\Sitemap\SitemapCron::class))
                 ->name('SitemapCron')
                 ->daily()
                 ->runInBackground();
 
-            // $schedule->call($this->app->make(\N1ebieski\ICore\Crons\Tag\Post\PopularTagsCron::class))
+            // $this->schedule->call($this->app->make(\N1ebieski\ICore\Crons\Tag\Post\PopularTagsCron::class))
             //     ->name('Post.PopularTagsCron')
             //     ->daily()
             //     ->runInBackground();
