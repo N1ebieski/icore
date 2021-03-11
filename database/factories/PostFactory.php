03955db3e736d52a896dcaef62fcd570034446de
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-03-11 12:17:08 +0100

    faker images in posts

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,13 +6,12 @@ use N1ebieski\ICore\Models\Post;
 use Carbon\Carbon;
 
 $factory->define(Post::class, function (Faker $faker) {
-
     $content = $faker->text(2000);
 
     return [
         'title' => $faker->sentence(5),
         'content_html' => $content,
-        'content' => $content,
+        'content' => strip_tags($content),
         'seo_title' => $faker->randomElement([$faker->sentence(5), null]),
         'seo_desc' => $faker->text(),
         'seo_noindex' => rand(0, 1),
@@ -23,6 +22,31 @@ $factory->define(Post::class, function (Faker $faker) {
             $faker->dateTimeThisYear()->format('Y-m-d H:i:s'),
             null
         ]),
+    ];
+});
+
+$factory->state(Post::class, 'image', function (Faker $faker) {
+    $content = $faker->text(2000);
+
+    $faker->addProvider(new \Mmo\Faker\PicsumProvider($faker));
+    $split = explode('. ', $content);
+    $rands = array_rand(array_slice($split, 0, array_key_last($split) - 5), rand(1, 3));
+
+    $split = array_map(function ($item) use ($split) {
+        return $item . ($item !== end($split) ? '.' : null);
+    }, $split);
+
+    foreach ($rands as $rand) {
+        $image = ['</p><p><img src="' . $faker->picsumUrl(1920, 1080) .'"></p><p>'];
+
+        array_splice($split, $rand, 0, $image);
+    }
+
+    $content = '<p>' . implode(' ', $split) . '</p>';
+
+    return [
+        'content_html' => $content,
+        'content' => strip_tags($content),
     ];
 });
 
--- database/seeds/Env/PostsSeeder.php
+++ database/seeds/Env/PostsSeeder.php
@@ -25,7 +25,12 @@ class PostsSeeder extends Seeder
         factory(User::class, 100)->states('user')->create()
             ->each(function ($u) use ($categories) {
                 for ($i=0; $i<rand(0, 5); $i++) {
-                    $post = $u->posts()->save(factory(Post::class)->make());
+                    $post = $u->posts()
+                        ->save(
+                            rand(0, 1) === 1 ?
+                                factory(Post::class)->states('image')->make()
+                                : factory(Post::class)->make()
+                        );
                     $post->tag(Faker::create()->words(rand(1, 5)));
                     shuffle($categories);
                     $post->categories()->attach(array_slice($categories, 0, rand(1, 5)));
