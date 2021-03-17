7e330bd15d150e77e32ff3f952324666aa96c73a
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-03-17 16:45:43 +0100

    lazy load placeholder

--- resources/js/web/scripts/plugins/jquery-lazy.js
+++ resources/js/web/scripts/plugins/jquery-lazy.js
@@ -2,6 +2,7 @@ jQuery(document).on('readyAndAjax', function () {
     $('.lazy').lazy({
         effect: "fadeIn",
         effectTime: "fast",
-        threshold: 0
+        threshold: 0,
+        placeholder: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
     });
 });
