07e038ed2a8d0e71acdebe1e99f006b973df514e
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-04-12 18:51:02 +0200

    responsive fixes

--- resources/views/web/partials/nav.blade.php
+++ resources/views/web/partials/nav.blade.php
@@ -80,7 +80,7 @@
         @endif
         <div class="navbar-collapse scroll collapse flex-grow-0 justify-content-end">
             @render('icore::page.menuComponent', ['limit' => 5])
-            <ul class="navbar-nav pr-3 pr-md-0 pb-5 p-sm-0">
+            <ul class="navbar-nav pr-3 pr-md-0 pb-5 p-md-0">
                 @if (!$isUrl(route('web.home.index')))
                 <li class="nav-item d-none d-md-inline mr-1">
                     @yield('search-toggler')
