04e379f28426e18f8e2205f3d488849fb4186282
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-07-23 15:21:38 +0200

    version

--- src/Providers/ICoreServiceProvider.php
+++ src/Providers/ICoreServiceProvider.php
@@ -13,7 +13,7 @@ class ICoreServiceProvider extends ServiceProvider
      * [public description]
      * @var string
      */
-    public const VERSION = "2.4.2";
+    public const VERSION = "2.4.3";
 
     /**
      * Register services.
