796cab3a83f7ceb445e0092958ed0458ad528585
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-08-14 00:56:51 +0200

    add possibility injection parameters instead array

--- src/Utils/Traits/Factory.php
+++ src/Utils/Traits/Factory.php
@@ -8,16 +8,25 @@ trait Factory
      * Undocumented function
      *
      * @param array ...$parameters
-     * @return self
+     * @return static
      */
-    public function make(array $parameters)
+    public function make(...$parameters)
     {
         $class = new \ReflectionClass(static::class);
 
         $newParameters = [];
+        $i = 0;
 
         foreach ($class->getConstructor()->getParameters() as $parameter) {
-            $newParameters[] = $parameters[$parameter->name] ?? $this->{$parameter->name};
+            if (isset($parameters[0]) && is_array($parameters[0]) && isset($parameters[0][$parameter->name])) {
+                $newParameters[] = $parameters[0][$parameter->name];
+            } else if (isset($this->{$parameter->name})) {
+                $newParameters[] = $this->{$parameter->name};
+            } else {
+                $newParameters[] = $parameters[$i];
+
+                $i++;
+            }
         }
 
         return new static(...$newParameters);
