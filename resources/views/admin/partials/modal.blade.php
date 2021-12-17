e5a7005bb5d7005d840015c0be06884325dde6ad
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-12-17 14:34:23 +0100

    refactoring

--- resources/views/admin/partials/modal.blade.php
+++ resources/views/admin/partials/modal.blade.php
@@ -3,7 +3,7 @@
     id="{{ $modal_id }}" 
     tabindex="-1" 
     role="dialog"
-    aria-labelledby="{{ $modal_id }}Title" 
+    aria-labelledby="{{ $modal_id }}-title" 
     aria-hidden="true" 
     data-focus="false"
 >
@@ -13,7 +13,7 @@
     >
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="{{ $modal_id }}Title">
+                <h5 class="modal-title" id="{{ $modal_id }}-title">
                     {{ $modal_title }}
                 </h5>
                 <button 
