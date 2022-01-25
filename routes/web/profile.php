9062ce6e553449a3489da78f70f84b754af3b936
N1ebieski <mariusz.wysokinski@neostrada.pl>
2022-01-25 23:19:32 +0100

    refactoring

--- routes/web/profile.php
+++ routes/web/profile.php
@@ -5,13 +5,19 @@ use N1ebieski\ICore\Http\Controllers\Web\Profile\ProfileController;
 use N1ebieski\ICore\Http\Controllers\Web\Profile\SocialiteController;
 
 Route::group(['middleware' => 'auth'], function () {
+    Route::get('profile/socialites', [ProfileController::class, 'socialites'])
+        ->name('profile.socialites');
+
     Route::get('symlink/{provider}', [SocialiteController::class, 'redirect'])
-        ->name('profile.socialite.redirect')->where('provider', '[A-Za-z]+');
+        ->name('profile.socialite.redirect')
+        ->where('provider', '[A-Za-z]+');
     Route::get('symlink/{provider}/callback', [SocialiteController::class, 'callback'])
-        ->name('profile.socialite.callback')->where('provider', '[A-Za-z]+');
+        ->name('profile.socialite.callback')
+        ->where('provider', '[A-Za-z]+');
 
     Route::delete('symlink/socialites/{socialite}', [SocialiteController::class, 'destroy'])
-        ->name('profile.socialite.destroy')->middleware('can:delete,socialite')
+        ->name('profile.socialite.destroy')
+        ->middleware('can:delete,socialite')
         ->where('socialite', '[0-9]+');
 
     Route::get('profile/edit', [ProfileController::class, 'edit'])
@@ -20,11 +26,9 @@ Route::group(['middleware' => 'auth'], function () {
         ->name('profile.update');
 
     Route::get('profile/edit/password', [ProfileController::class, 'redirectPassword'])
-        ->name('profile.redirect_password')->middleware('verified');
+        ->name('profile.redirect_password')
+        ->middleware('verified');
 
     Route::patch('profile/email', [ProfileController::class, 'updateEmail'])
         ->name('profile.update_email');
-
-    Route::get('profile/socialites', [ProfileController::class, 'socialites'])
-        ->name('profile.socialites');        
 });
