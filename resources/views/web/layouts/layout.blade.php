40d6833d325158022543bbb39e2cf96626b067cd
N1ebieski <mariusz.wysokinski@neostrada.pl>
2022-05-25 12:58:19 +0200

    apple-touch-icon

--- resources/views/web/layouts/layout.blade.php
+++ resources/views/web/layouts/layout.blade.php
@@ -20,6 +20,7 @@
 
     <link rel="canonical" href="{{ $getUrl }}">
     <link rel="icon" href="{{ asset('svg/vendor/icore/logo.svg') }}" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="{{ asset('images/vendor/icore/logo.png') }}">      
     <link href="{{ asset(mix('css/vendor/icore/vendor/vendor.css')) }}" rel="stylesheet">
     <link href="{{ asset(mix($getStylesheet())) }}" rel="stylesheet">
     <link href="{{ asset($getStylesheet('css/custom')) }}" rel="stylesheet">
