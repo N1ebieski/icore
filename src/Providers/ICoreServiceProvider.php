8a7ec5370c3d3a37d04dadf982d3f3559c95af30
N1ebieski <kontakt@intelekt.net.pl>
2022-10-07 02:02:24 +0200

    version

--- src/Providers/ICoreServiceProvider.php
+++ src/Providers/ICoreServiceProvider.php
@@ -27,7 +27,7 @@ class ICoreServiceProvider extends ServiceProvider
      * [public description]
      * @var  string
      */
-    public const VERSION = "8.0.0";
+    public const VERSION = "8.0.0-rc.1";
 
     /**
      * Register services.
