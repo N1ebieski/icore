e9bcd282dfb5f0c35cf2dc543e92b28f526b06e8
N1ebieski <kontakt@intelekt.net.pl>
2022-10-20 22:28:33 +0000

    fix schedule

--- src/Providers/ICoreServiceProvider.php
+++ src/Providers/ICoreServiceProvider.php
@@ -27,7 +27,7 @@ class ICoreServiceProvider extends ServiceProvider
      * [public description]
      * @var  string
      */
-    public const VERSION = "8.1.0-rc.1";
+    public const VERSION = "8.1.0-rc.2";
 
     /**
      * Register services.
