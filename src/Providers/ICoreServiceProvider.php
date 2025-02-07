f4a92017351279ddd6d5cf799e070543be0da23e
N1ebieski <kontakt@intelekt.net.pl>
2025-02-07 14:20:07 +0000

    fix for short names of categories

--- src/Providers/ICoreServiceProvider.php
+++ src/Providers/ICoreServiceProvider.php
@@ -27,7 +27,7 @@ class ICoreServiceProvider extends ServiceProvider
      * [public description]
      * @var  string
      */
-    public const VERSION = "8.3.2";
+    public const VERSION = "8.3.3";
 
     /**
      * Register services.
