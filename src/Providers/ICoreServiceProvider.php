2f5e148d468b7f6c524e5dfd4d2e2532c28304c0
N1ebieski <kontakt@intelekt.net.pl>
2025-10-28 07:42:38 +0000

    fix typo

--- src/Providers/ICoreServiceProvider.php
+++ src/Providers/ICoreServiceProvider.php
@@ -27,7 +27,7 @@ class ICoreServiceProvider extends ServiceProvider
      * [public description]
      * @var  string
      */
-    public const VERSION = "8.4.1";
+    public const VERSION = "8.4.2";
 
     /**
      * Register services.
